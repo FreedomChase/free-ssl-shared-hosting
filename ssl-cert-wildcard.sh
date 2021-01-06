5344c347cb0bd7a0963dbdcb2dd3018f58ada1b7
FreedomChase <freedom.chase@hotmail.com>
2021-01-06 14:10:31 +0100

    Removed test domain name from shell script

--- ssl-cert-wildcard.sh
+++ ssl-cert-wildcard.sh
@@ -8,4 +8,4 @@ sudo apt update
 sudo apt install certbot
 
 # Generate the cert
-sudo certbot certonly --manual -d *.isc.coop -d isc.coop --agree-tos --no-bootstrap --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+sudo certbot certonly --manual -d *.example.com -d example.com --agree-tos --no-bootstrap --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
